0a6222e7fe9e0f070cc5e8223a9eaf98a5e23712
arthur hodosh <arthurhodosh2011@gmail.com>
2022-07-31 23:31:21 +0300

    add classes to work with social networks

--- flask_app/src/project/api/v1/auth/routes.py
+++ flask_app/src/project/api/v1/auth/routes.py
@@ -1,18 +1,15 @@
 from datetime import timedelta
 from http import HTTPStatus
-from urllib.parse import urlencode
 
-import requests
 from apifairy import response, body
-from flask import abort, url_for, session, redirect, request
+from flask import abort, url_for, redirect
 from flask_jwt_extended import create_access_token, get_jwt, jwt_required
-from requests import post
 
 from project.core.config import settings
 from project.extensions import jwt_redis_blocklist, log_activity
 from project.models.models import User
 from project.schemas import token_schema, message_schema, login_schema
-from project.services.social_auth import oauth, google_client
+from project.services.social_auth import ExternalAuthActions
 from . import auth_api_blueprint
 
 
@@ -59,53 +56,13 @@ def logout():
 @auth_api_blueprint.route('/login/<provider>', methods=['GET'])
 def login_provider(provider: str):
     """Login with Google+Yandex"""
-    if provider.lower() == 'google':
-        redirect_uri = url_for('auth.authorize_google', _external=True)
-        return google_client.authorize_redirect(redirect_uri)
-
-    elif provider.lower() == 'yandex':
-        return redirect(
-            settings.YANDEX_BASE_URL + f'authorize?response_type=code&client_id={settings.YANDEX_CLIENT_ID}')
-
-    else:
-        abort(HTTPStatus.NOT_FOUND, f'wrong type of provider: {provider}')
+    ExternalAuthActions.login_redirect(provider)
 
 
 @auth_api_blueprint.route('/authorize/<provider>', methods=['GET'])
 def authorize(provider: str):
     """Authorize with Google+Yandex"""
-    userinfo = ''
-    email = ''
-
-    if provider.lower() == 'google':
-        token = google_client.authorize_access_token()  # Access token from google (needed to get user info)
-        userinfo = oauth.google.userinfo()
-        email = userinfo['email']
-
-    elif provider.lower() == 'yandex':
-        data = {
-            'grant_type': 'authorization_code',
-            'code': request.args.get('code'),
-            'client_id': settings.YANDEX_CLIENT_ID,
-            'client_secret': settings.GOOGLE_CLIENT_SECRET,
-        }
-        data = urlencode(data)
-        resp = post(settings.YANDEX_BASE_URL + 'token', data).json()
-        # Токен необходимо сохранить для использования в запросах к API
-        access_token: str = resp.get('access_token')
-        userinfo = requests.get(
-            url=settings.YANDEX_LOGIN_INFO_URL,
-            params={
-                "format": "json",
-                "with_openid_identity": 1,
-                "oauth_token": access_token,
-            },
-        ).json()
-        email = userinfo['default_email']
-
-    else:
-        abort(HTTPStatus.NOT_FOUND, f'wrong type of provider: {provider}')
-
+    email = ExternalAuthActions.check_email(provider)
     # check user in database
     user = User.query.filter_by(email=email, disabled=False).first()
 
@@ -113,8 +70,6 @@ def authorize(provider: str):
         reg_url = url_for('users.register')
         return redirect(reg_url, 302)
 
-    session['profile'] = userinfo
-    session.permanent = True  # make the session permanant so it keeps existing after browser gets closed
     additional_claims = {'role_id': user.role_id}
     access_token = create_access_token(identity=email, additional_claims=additional_claims)
     log_activity(user.id, f'login with {provider}')
--- flask_app/src/project/services/social_auth.py
+++ flask_app/src/project/services/social_auth.py
@@ -1,4 +1,11 @@
+from abc import ABC, abstractmethod
+from http import HTTPStatus
+from urllib.parse import urlencode
+
+import requests
 from authlib.integrations.flask_client import OAuth
+from flask import url_for, request, redirect, abort
+from requests import post
 
 from app import app
 from project import settings
@@ -23,3 +30,97 @@ google = oauth.register(
 google_client = oauth.create_client('google')
 
 
+class BaseProvider(ABC):
+    def __init__(self, provider_name: str):
+        self.provider_name = provider_name
+
+    @abstractmethod
+    def login_redirect(self):
+        pass
+
+    @abstractmethod
+    def check_response_and_get_email(self):
+        pass
+
+
+class GoogleProvider(BaseProvider):
+    def __init__(self, provider_name: str):
+        super().__init__(provider_name)
+        self.provider_name = provider_name
+
+    @staticmethod
+    def login_redirect():
+        return redirect(
+            settings.YANDEX_BASE_URL + f'authorize?response_type=code&client_id={settings.YANDEX_CLIENT_ID}')
+
+    @staticmethod
+    def check_response_and_get_email() -> str:
+        token = google_client.authorize_access_token()  # Access token from google (needed to get user info)
+        userinfo = oauth.google.userinfo()
+        return userinfo['email']
+
+
+class YandexProvider(BaseProvider):
+    def __init__(self, provider_name: str):
+        super().__init__(provider_name)
+        self.provider_name = provider_name
+
+    @staticmethod
+    def login_redirect():
+        redirect_uri = url_for('auth.authorize_google', _external=True)
+        return google_client.authorize_redirect(redirect_uri)
+
+    @staticmethod
+    def check_response_and_get_email() -> str:
+        data = {
+            'grant_type': 'authorization_code',
+            'code': request.args.get('code'),
+            'client_id': settings.YANDEX_CLIENT_ID,
+            'client_secret': settings.GOOGLE_CLIENT_SECRET,
+        }
+        data = urlencode(data)
+        resp = post(settings.YANDEX_BASE_URL + 'token', data).json()
+        # Токен необходимо сохранить для использования в запросах к API
+        access_token: str = resp.get('access_token')
+        userinfo = requests.get(
+            url=settings.YANDEX_LOGIN_INFO_URL,
+            params={
+                "format": "json",
+                "with_openid_identity": 1,
+                "oauth_token": access_token,
+            },
+        ).json()
+        return userinfo['default_email']
+
+
+class ExternalAuthActions:
+
+    @staticmethod
+    def get_provider_instance(provider_name: str) -> BaseProvider:
+        """
+        Метод для получения класса конкретного провайдера по его имени
+        """
+        if provider_name.lower() == 'google':
+            return GoogleProvider
+
+        elif provider_name.lower() == 'yandex':
+            return YandexProvider
+
+        else:
+            abort(HTTPStatus.NOT_FOUND, f'wrong type of provider: {provider_name}')
+
+    @staticmethod
+    def login_redirect(provider_name: str):
+        """
+        Метод перенаправляет логин на внешний сервис по имени провайдера
+        """
+        provider = ExternalAuthActions.get_provider_instance(provider_name)
+        return provider.login_redirect()
+
+    @staticmethod
+    def check_email(provider_name: str):
+        """
+        Метод проверяет ответ (его правомерность) и возвращает почтовый ящик
+        """
+        provider = ExternalAuthActions.get_provider_instance(provider_name)
+        return provider.check_response_and_get_email()
