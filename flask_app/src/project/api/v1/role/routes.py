7068fbbb8ab740b34d15535f21445cb7af67f03d
arthur hodosh <arthurhodosh2011@gmail.com>
2022-08-06 10:58:17 +0300

    replace codes with HTTPStatus

--- flask_app/src/project/api/v1/role/routes.py
+++ flask_app/src/project/api/v1/role/routes.py
@@ -17,7 +17,7 @@ from . import role_api_blueprint
 @role_api_blueprint.route('/create', methods=['POST'])
 @jwt_required()
 @body(new_role_schema)
-@response(role_schema, 200)
+@response(role_schema, HTTPStatus.OK)
 @rate_limit(by_email=True, by_ip=True)
 @check_access([ROLE_SELF.CREATE, ROLE_ALL.CREATE])
 def create_role(kwargs: dict):
@@ -44,7 +44,7 @@ def create_role(kwargs: dict):
 @role_api_blueprint.route('/<role_id>', methods=['POST'])
 @jwt_required()
 @body(new_role_schema)
-@response(role_schema, 200)
+@response(role_schema, HTTPStatus.OK)
 @rate_limit(by_email=True, by_ip=True)
 @check_access([ROLE_SELF.UPDATE, ROLE_ALL.UPDATE])
 def update_role(kwargs: dict, role_id: str):
@@ -69,7 +69,7 @@ def update_role(kwargs: dict, role_id: str):
 
 @role_api_blueprint.route('/', methods=['GET'])
 @jwt_required()
-@response(ShortRoleSchema(many=True), 200)
+@response(ShortRoleSchema(many=True), HTTPStatus.OK)
 @rate_limit(by_email=True, by_ip=True)
 @check_access([ROLE_SELF.READ, ROLE_ALL.READ])
 def get_all_roles():
@@ -83,7 +83,7 @@ def get_all_roles():
 
 @role_api_blueprint.route('/<role_id>', methods=['GET'])
 @jwt_required()
-@response(role_schema, 200)
+@response(role_schema, HTTPStatus.OK)
 @rate_limit(by_email=True, by_ip=True)
 @check_access([ROLE_SELF.READ, ROLE_ALL.READ])
 def get_role(role_id: str):
@@ -99,7 +99,7 @@ def get_role(role_id: str):
 
 @role_api_blueprint.route('/<role_id>', methods=['DELETE'])
 @jwt_required()
-@response(role_schema, 200)
+@response(role_schema, HTTPStatus.OK)
 @rate_limit(by_email=True, by_ip=True)
 @check_access([ROLE_SELF.DELETE, ROLE_ALL.DELETE])
 def delete_role(role_id: str):
